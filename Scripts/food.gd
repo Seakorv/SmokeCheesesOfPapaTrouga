dbd80b2e30d5a2b11ecaffd9da872d06a3efde00
aojuntun <arttu.o.juntunen@student.jyu.fi>
2024-02-02 16:36:54 +0200

    replaced bad food with general class Food

--- Scripts/food.gd
+++ Scripts/food.gd
@@ -1,6 +1,7 @@
-class_name JellyOnions extends Area2D
+class_name Food extends Area2D
 
-@export var speed = 400
+@export var speed = 500
+
 
 func _physics_process(delta):
 	global_position.x += -speed * delta
@@ -8,9 +9,8 @@ func _physics_process(delta):
 
 func die():
 	queue_free()
-
+	
 
 func _on_body_entered(body):
 	if body is PapaT:
-		body.die()
 		die()
--- Scripts/gee_bullet.gd
+++ Scripts/gee_bullet.gd
@@ -12,6 +12,6 @@ func _on_visible_on_screen_notifier_2d_screen_exited():
 
 
 func _on_area_entered(area):
-	if area is Olives or JellyOnions:
+	if area is Food:
 		area.die()
 		queue_free()
